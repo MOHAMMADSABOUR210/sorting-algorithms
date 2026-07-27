0f0913cda076e4eb935ffee5fecb9a8233355bd5
MOHAMMAD SABOUR <m210saboor@gmail.com>
2026-07-27 15:16:55 +0330

    feat: add merge sort algorithm

---
+++ algorithms/comparison/Marg_sort.py
@@ -0,0 +1,27 @@
+from typing import TypeVar
+
+T = TypeVar("T")
+
+def merge_sort(arr: list[T]) -> list[T]:
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged
